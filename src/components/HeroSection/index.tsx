1fc42c93a3e462b1ccd323ee733725f1208366ae
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-07-23 18:38:29 +0400

    chore: html structure created

---
+++ src/components/HeroSection/index.tsx
@@ -0,0 +1,14 @@
+export default function HeroSection() {
+
+
+    return (
+        <div>
+            <h1>Freshly Baked, <br /> Daily.</h1>
+            <p>Experience the soul of French artisanal baking. <br />
+                Handcrafted with organic heritage grains and a century of tradition.</p>
+            <button></button>
+            <button></button>
+            <button></button>
+        </div>
+    )
+}

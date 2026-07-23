f4f0ebb2586fe15f9a82a22403788bb3a84e94d5
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-07-23 19:08:57 +0400

    chore:coresponding languages added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useTranslation } from 'react-i18next'
 
 function App() {
   const { t } = useTranslation()
--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,14 +1,19 @@
+import styles from './style.module.css';
+import { useTranslation } from 'react-i18next'
+
+
 export default function HeroSection() {
 
+    const { t } = useTranslation();
 
     return (
-        <div>
-            <h1>Freshly Baked, <br /> Daily.</h1>
-            <p>Experience the soul of French artisanal baking. <br />
-                Handcrafted with organic heritage grains and a century of tradition.</p>
-            <button></button>
-            <button></button>
-            <button></button>
+        <div className={styles.container}>
+            <h1>{t('heroSection.header-1st-line')} <br />{t('heroSection.header-2nd-line')}</h1>
+            <p>{t('heroSection.p-1st-line')} <br />
+                {t('heroSection.p-2nd-line')}</p>
+            <button>{t('heroSection.btn-order-pickup')}</button>
+            <button>{t('heroSection.btn-delivery')}</button>
+            <button>{t('heroSection.btn-reservation')} </button>
         </div>
     )
 }

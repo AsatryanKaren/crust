404f7e5f282d0f76345ec5437d861f7b3232e84e
sona2711 <sonamkrtchyan2711@gmail.com>
2026-08-07 22:39:35 +0400

    Replace BrandName text with Logo.png and top-align it in the footer.

--- src/components/_shared/BrandName/index.tsx
+++ src/components/_shared/BrandName/index.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 
+import logo from '../../../assets/images/Logo.png'
 import { paths } from '../../../routes/paths'
 import type { Props } from './types'
 import styles from './styles.module.css'
@@ -10,7 +11,7 @@ export const BrandName: Props = () => {
 
   return (
     <Link className={styles.root} to={paths.home}>
-      {t('brand')}
+      <img className={styles.logo} src={logo} alt={t('brand')} />
     </Link>
   )
 }

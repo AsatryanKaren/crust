6bda9be170325a8330163a9234098ddd28649603
shoghdev <shogher.fordev@gmail.com>
2026-08-08 12:32:09 +0400

    Replace the homePgae files

--- src/pages/Home/WhatWeOfferSection/consts.ts
+++ src/pages/Home/WhatWeOfferSection/consts.ts
@@ -1,7 +1,7 @@
-import breadsImage from '../../assets/images/WhatWeOfferSection/breads.jpg'
-import pastriesImage from '../../assets/images/WhatWeOfferSection/pastries.jpg'
-import cakesImage from '../../assets/images/WhatWeOfferSection/cakes.jpg'
-import cocktailsImage from '../../assets/images/WhatWeOfferSection/cocktails.jpg'
+import breadsImage from '../../../assets/images/WhatWeOfferSection/breads.jpg'
+import pastriesImage from '../../../assets/images/WhatWeOfferSection/pastries.jpg'
+import cakesImage from '../../../assets/images/WhatWeOfferSection/cakes.jpg'
+import cocktailsImage from '../../../assets/images/WhatWeOfferSection/cocktails.jpg'
 
 export const CATEGORY_IDS = ['breads', 'pastries', 'cakes', 'cocktails'] as const
 
